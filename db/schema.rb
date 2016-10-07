f2c187b99192726a656442316140d44022f916e1
marc desrosiers <mdesrosiers@norauto.fr>
2016-10-07 18:29:17 +0200

    create model Category and list of days

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930211939) do
+ActiveRecord::Schema.define(version: 20161007162749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,18 @@ ActiveRecord::Schema.define(version: 20160930211939) do
     t.integer  "user_id"
     t.index ["pizza_id"], name: "index_carts_on_pizza_id", using: :btree
     t.index ["user_id"], name: "index_carts_on_user_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "days", force: :cascade do |t|
+    t.string   "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
