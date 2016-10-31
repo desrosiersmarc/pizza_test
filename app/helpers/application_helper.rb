9f465aa0e521911b1648536e44359adf32cb1c74
marc desrosiers <mdesrosiers@norauto.fr>
2016-10-31 12:47:55 +0100

    Work on css for foodtruck

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     "http://wire.kapitall.com/wp-content/image-import/healthy-food-stocks.jpeg"
   end
 
+  def helper_picture_url(food_truck)
+    if food_truck.photo?
+      cl_image_path(food_truck.photo.path)
+    else
+      "https://static.pexels.com/photos/162971/potatoes-french-mourning-funny-162971.jpeg"
+    end
+  end
+
 end
