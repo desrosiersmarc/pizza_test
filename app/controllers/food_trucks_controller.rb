cabb3349c506172c0aef621a99e6e9023ef8fb98
marc desrosiers <mdesrosiers@norauto.fr>
2016-11-05 15:11:40 +0100

    Modify food_trucks_controller to remove query+1 for index

--- app/controllers/food_trucks_controller.rb
+++ app/controllers/food_trucks_controller.rb
@@ -3,9 +3,10 @@ class FoodTrucksController < ApplicationController
 
   def index
     @city = params[:search][:city]
-    @food_trucks = FoodTruck.all
-    @open_days = OpenDay.where("day_id = ?", Day.find_by_day_of_week(Time.now.strftime("%w")).id)
-                        .near(@city, 200)
+    @opendays = OpenDay.includes(food_truck:[:category]).near(@city, 200)
+                    .where(open_days: {day_id: Day.find_by_day_of_week(Time.now.strftime("%w")).id })
+
+
   end
 
   def show
