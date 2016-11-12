fbc16f6ce7e79ddecf51d96e265a1f7e5295e477
marc desrosiers <mdesrosiers@norauto.fr>
2016-11-12 23:42:57 +0100

    work to add i18n and change the logo of navbar for connect

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,12 @@ require "attachinary/orm/active_record"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module PizzaTest
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.default_locale = :fr
+  config.i18n.default_locale = :fr
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   mount Attachinary::Engine => "/attachinary"
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pizzas, only: [:index, :new, :create] do
-    resources :ingredients, only: [:index, :show]
-  end
-  resources :carts
-  resources :food_trucks, only: [:index, :new, :create] do
-    resources :open_days, only: [:index, :new, :create, :destroy]
+
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :pizzas, only: [:index, :new, :create] do
+      resources :ingredients, only: [:index, :show]
+    end
+    resources :carts
+    resources :food_trucks, only: [:index, :new, :create] do
+      resources :open_days, only: [:index, :new, :create, :destroy]
+    end
   end
 end
